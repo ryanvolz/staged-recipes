5bcc6ea42df1341e08785b052e2c26453a59158f
Ryan Volz <rvolz@mit.edu>
2020-06-08 18:31:37 -0400

    Add rtl-sdr.

---
+++ recipes/rtl-sdr/bld.bat
@@ -0,0 +1,29 @@
+setlocal EnableDelayedExpansion
+@echo on
+
+:: Make a build folder and change to it
+mkdir build
+cd build
+
+:: configure
+:: change -DCMAKE_INSTALL_LIBDIR:PATH="lib" to -DCMAKE_INSTALL_LIBDIR:PATH="lib" after 0.6
+cmake -G "Ninja" ^
+    -DCMAKE_BUILD_TYPE:STRING=Release ^
+    -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+    -DLIB_INSTALL_DIR:PATH="lib" ^
+    -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+    -DLIBUSB_INCLUDE_DIR:PATH="%LIBRARY_INC%\libusb-1.0" ^
+    -DTHREADS_PTHREADS_WIN32_LIBRARY:FILEPATH="%LIBRARY_LIB%\pthread.lib" ^
+    -DDETACH_KERNEL_DRIVER=OFF ^
+    -DENABLE_ZEROCOPY=OFF ^
+    -DINSTALL_UDEV_RULES=OFF ^
+    ..
+if errorlevel 1 exit 1
+
+:: build
+cmake --build . --config Release -- -j%CPU_COUNT%
+if errorlevel 1 exit 1
+
+:: install
+cmake --build . --config Release --target install
+if errorlevel 1 exit 1
